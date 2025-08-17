abf0d06df852957a46f2caf6d8053327d21a6c19
justine <enodd_edc@protonmail.com>
2025-08-17 15:44:38 +0800

    Fix file structure

--- src/app/api/auth/login/route.js
+++ src/app/api/auth/login/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import connectMongoDB from '@/lib/mongodb';
+import connectMongoDB from '@/config/mongoConfig';
 import User from '@/models/User';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
--- src/app/api/auth/register/route.js
+++ src/app/api/auth/register/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import connectMongoDB from '@/lib/mongodb';
+import connectMongoDB from '@/config/mongoConfig';
 import User from '@/models/User';
 import bcrypt from 'bcryptjs';
 import nodemailer from 'nodemailer';
