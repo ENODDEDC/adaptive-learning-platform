50838089a88b9e7422df84c3b8d9cb799c6af970
justine <enodd_edc@protonmail.com>
2025-08-26 21:46:40 +0800

    FIX Extra layout component

--- src/app/text-to-docs/page.js
+++ src/app/text-to-docs/page.js
@@ -1,13 +1,10 @@
 import { Suspense } from 'react';
 import TextToDocsClient from '@/components/TextToDocsClient';
-import Layout from '@/components/Layout';
 
 export default function TextToDocsPage() {
   return (
-    <Layout>
-      <Suspense fallback={<div>Loading...</div>}>
-        <TextToDocsClient />
-      </Suspense>
-    </Layout>
+    <Suspense fallback={<div>Loading...</div>}>
+      <TextToDocsClient />
+    </Suspense>
   );
 }
