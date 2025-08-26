8965cef832f140e75bf29807ddcce72cbf019799
justine <enodd_edc@protonmail.com>
2025-08-26 17:37:10 +0800

    Fix ESLint errors

--- src/components/AdminNavbar.js
+++ src/components/AdminNavbar.js
@@ -39,10 +39,12 @@ export default function AdminNavbar({ toggleSidebar }) {
             <Menu.Button className="relative flex items-center max-w-xs text-sm bg-gray-800 rounded-full focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
               <span className="absolute -inset-1.5" />
               <span className="sr-only">Open user menu</span>
-              <img
+              <Image
                 className="w-8 h-8 rounded-full"
                 src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                 alt=""
+                width={32}
+                height={32}
               />
               <span className="ml-2 text-white">Admin Name</span> {/* Replace with actual admin name */}
               <ChevronDownIcon className="w-5 h-5 ml-1 text-gray-400" aria-hidden="true" />
